226d1e0d0100c06469db83f43378b6f1af70627b
archbuilder-git <189096068+archbuilder-git@users.noreply.github.com>
2025-03-15 11:34:44 +0200

    update

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -8,14 +8,14 @@ repo-add -s -n -R archpro_repo.db.tar.gz *.pkg.tar.zst
 sleep 1
 
 rm archpro_repo.db
-rm archpro_repo.db.sig
-
 rm archpro_repo.files
-rm archpro_repo.files.sig
 
 mv archpro_repo.db.tar.gz archpro_repo.db
 mv archpro_repo.files.tar.gz archpro_repo.files
 
+# Preserve and rename the signature files
+mv archpro_repo.db.tar.gz.sig archpro_repo.db.sig 2>/dev/null
+mv archpro_repo.files.tar.gz.sig archpro_repo.files.sig 2>/dev/null
 
 echo "####################################"
 echo "Repo Updated!!"
